04e606b6635286669bc227a11f2a43fca530d40d
adiatraa <adiatrahman31@gmail.com>
2024-08-27 11:50:50 +0700

    Add Hotel Detail Page

---
+++ staylio/src/app/(withNavbar)/hotel/[hotelName]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Page({ params }: { params: { hotelName: string}} ) {
+  return (
+    <div>
+        {/* BreadCrumb Section */}
+        <div className='w-full h-[150px] bg-[#FE6927] rounded-b-3xl'>
+
+        </div>
+    </div>
+  )
+}
